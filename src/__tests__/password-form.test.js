67514560d3524fe9c44b47981a20056771d31c96
Kevin Barabash <kevinb7@gmail.com>
2020-07-24 15:13:17 -0400

    Test the password machine directly by sending different events and checking the state (#2)

--- src/__tests__/password-form.test.js
+++ src/__tests__/password-form.test.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import {mount} from "enzyme";
+import { interpret } from "xstate";
 
-import {passwordFormModel} from "../password-form-machine";
+import {passwordFormModel, passwordFormMachine} from "../password-form-machine";
 import PasswordForm from "../password-form";
 
-describe.only('PasswordForm', () => {
+describe('PasswordForm', () => {
   const testPlans = passwordFormModel.getShortestPathPlans();
 
   testPlans.forEach(plan => {
@@ -23,6 +24,44 @@ describe.only('PasswordForm', () => {
     return passwordFormModel.testCoverage({
       // ignore transient check_passwords transitions
       filter: stateNode => !!stateNode.meta,
+    });
+  });
+
+  describe("service", () => {
+    it("starts in the initial state", () => {
+      const service = interpret(passwordFormMachine);
+      service.start();
+      
+      expect(service.state.value).toEqual({"existingPassword": "start", "newPasswords": "start"});
+    });
+
+    it("only setting one password will result in 'not_equal' state", () => {
+      const service = interpret(passwordFormMachine);
+      service.start();
+
+      service.send("FIRST_PASSWORD", {value: "abc"});
+      
+      expect(service.state.value.newPasswords).toEqual("not_equal");
+    });
+
+    it("setting both passwords to '' will result in 'empty' state", () => {
+      const service = interpret(passwordFormMachine);
+      service.start();
+
+      service.send("FIRST_PASSWORD", {value: ""});
+      service.send("SECOND_PASSWORD", {value: ""});
+
+      expect(service.state.value.newPasswords).toEqual("empty");
+    });
+
+    it("setting both passwords to he same value is 'valid'", () => {
+      const service = interpret(passwordFormMachine);
+      service.start();
+
+      service.send("FIRST_PASSWORD", {value: "123"});
+      service.send("SECOND_PASSWORD", {value: "123"});
+
+      expect(service.state.value.newPasswords).toEqual("valid");
     });
   });
 });
